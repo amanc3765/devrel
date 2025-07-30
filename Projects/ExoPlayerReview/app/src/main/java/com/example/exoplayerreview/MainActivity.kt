4cf42591654a685f4d6b8ab97679134528de4af3
Aman Choudhary <amanc3765@gmail.com>
2025-07-30 14:14:17 +0530

    ExoPlayerReview: 2 - Basic ExoPlayer

--- Projects/ExoPlayerReview/app/src/main/java/com/example/exoplayerreview/MainActivity.kt
+++ Projects/ExoPlayerReview/app/src/main/java/com/example/exoplayerreview/MainActivity.kt
@@ -2,17 +2,38 @@ package com.example.exoplayerreview
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var playerView: PlayerView
+    private lateinit var player: ExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_main)
+        playerView = findViewById(R.id.playerView)
 
         initializePlayer()
     }
 
-    private fun initializePlayer() {
+    override fun onDestroy() {
+        super.onDestroy()
+        player.release()
+    }
 
+    private fun initializePlayer() {
+        player = ExoPlayer.Builder(this).build()
+
+        val mediaItem =
+            MediaItem.fromUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+        player.setMediaItem(mediaItem)
+
+        playerView.player = player
+        player.playWhenReady = true
+        player.prepare()
     }
 }
