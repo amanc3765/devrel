5df045d94c7bfcebac35764165c08fb73eb85a67
Aman Choudhary <amanc3765@gmail.com>
2025-08-03 15:42:45 +0530

    Added ExtractorTest

---
+++ Projects/MetadataRetrieverMedia3/app/src/main/java/com/example/metadataretrievermedia3/ExtractorTest.kt
@@ -0,0 +1,97 @@
+package com.example.metadataretrievermedia3
+
+import android.content.Context
+import android.media.MediaFormat
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.annotation.RequiresApi
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.MediaExtractorCompat
+import java.nio.ByteBuffer
+import java.util.Locale
+
+@OptIn(UnstableApi::class)
+@RequiresApi(Build.VERSION_CODES.O)
+class ExtractorTest(private val mediaPath: String, private val numIter: Int) {
+
+    fun startExtractorTest(context: Context): Long {
+        var totalTimeUs: Long = 0L
+        for (i in 0..<numIter) {
+            val currTimeUs = runExtractorMedia3(context)
+            totalTimeUs += currTimeUs
+            Log.d(
+                TAG, String.format(
+                    Locale.ROOT, "%30s %3d: %7d", mediaPath, i, currTimeUs
+                )
+            )
+        }
+
+        val meanTimeUs = totalTimeUs / numIter
+        Log.d(
+            TAG, String.format(
+                Locale.ROOT, "(single) %7d", meanTimeUs,
+            )
+        )
+
+        return meanTimeUs
+    }
+
+    private fun runExtractorMedia3(context: Context): Long {
+        val startTimeNs = System.nanoTime()
+
+        val extractor = MediaExtractorCompat(context)
+        val mediaUri: Uri = mediaPath.toUri()
+        extractor.setDataSource(mediaUri, 0L)
+
+        for (i in 0 until extractor.trackCount) {
+//            Log.i(TAG, "----- Track $i -----")
+            val mediaFormat: MediaFormat = extractor.getTrackFormat(i)
+//            Log.i(TAG, "$mediaFormat")
+
+            extractor.selectTrack(i)
+//            Log.i(TAG, "Selected track $i")
+        }
+
+        val metrics = extractor.metrics
+//        Log.i(TAG, "Metrics: $metrics")
+
+        extractor.seekTo(0L, MediaExtractorCompat.SEEK_TO_CLOSEST_SYNC)
+
+        val buffer = ByteBuffer.allocate(10 * 1024 * 1024)
+        while (true) {
+            val trackIndex = extractor.sampleTrackIndex
+            if (trackIndex < 0) break
+
+            val readBytes = extractor.readSampleData(buffer, 0)
+            if (readBytes < 0) break
+
+            val sampleTime: Long = extractor.sampleTime
+            val sampleFlags: Int = extractor.sampleFlags
+//            Log.i(
+//                TAG, "Track %2d: Read %8d bytes at %12d us, flags %d".format(
+//                    trackIndex, readBytes, sampleTime, sampleFlags
+//                )
+//            )
+
+            val cachedUs = extractor.cachedDuration
+//            Log.d(TAG, "Cached duration ahead: $cachedUs µs")
+
+            extractor.advance()
+        }
+
+        val isComplete = extractor.hasCacheReachedEndOfStream()
+//        if (isComplete) {
+//            Log.d(TAG, "All data is cached. End of stream reached.")
+//        }
+
+        extractor.release()
+//        Log.i(TAG, "Extractor released.")
+
+        return (System.nanoTime() - startTimeNs) / 1000
+    }
+}
--- Projects/MetadataRetrieverMedia3/app/src/main/java/com/example/metadataretrievermedia3/MainActivity.kt
+++ Projects/MetadataRetrieverMedia3/app/src/main/java/com/example/metadataretrievermedia3/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.metadataretrievermedia3
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
 
 const val TAG = "Media3Test"
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : ComponentActivity() {
 
     private val mediaPathList = mutableListOf(
@@ -15,12 +18,32 @@ class MainActivity : ComponentActivity() {
         "/sdcard/Download/mediadataset/file_example_MP4_1920_18MG.mp4",
     )
 
-    private var timeMap = mutableMapOf<String, MutableList<Long>>()
+
+    private var extractorTimeMap = mutableMapOf<String, MutableList<Long>>()
+    private var retrieverTimeMap = mutableMapOf<String, MutableList<Long>>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        retrieverTest()
+
+        extractorTest()
+//        retrieverTest()
+    }
+
+    private fun extractorTest() {
+        for (i in 0..<25) {
+            mediaPathList.shuffle()
+            mediaPathList.forEach { mediaPath ->
+                val meanTimeUs = ExtractorTest(mediaPath, 5).startExtractorTest(this)
+                extractorTimeMap.getOrPut(mediaPath) { mutableListOf() }.add(meanTimeUs)
+            }
+        }
+
+        extractorTimeMap.forEach { (mediaPath, timeList) ->
+            val meanTime = timeList.average()
+            Log.d(TAG, "Extraction time for $mediaPath: $meanTime µs")
+        }
     }
 
     private fun retrieverTest() {
@@ -29,13 +52,13 @@ class MainActivity : ComponentActivity() {
             mediaPathList.forEach { mediaPath ->
                 val performanceTest = PerformanceTest(this, mediaPath, 5)
                 val meanTimeUs = performanceTest.startMetadataRetrievalTest(this)
-                timeMap.getOrPut(mediaPath) { mutableListOf() }.add(meanTimeUs)
+                retrieverTimeMap.getOrPut(mediaPath) { mutableListOf() }.add(meanTimeUs)
             }
         }
 
-        timeMap.forEach { (mediaPath, timeList) ->
+        retrieverTimeMap.forEach { (mediaPath, timeList) ->
             val meanTime = timeList.average()
-            Log.d(TAG, "Mean time for $mediaPath: $meanTime µs")
+            Log.d(TAG, "Retriever time for $mediaPath: $meanTime µs")
         }
     }
 }
